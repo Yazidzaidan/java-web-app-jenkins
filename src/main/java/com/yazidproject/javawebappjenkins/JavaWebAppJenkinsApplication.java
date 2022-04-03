66eeac832df500305be7d9dffe35074e22658d8a
Yazidzaidan <yazidzaidanz@gmail.com>
2022-04-03 10:51:52 +0700

    How to Build and Deploy a Java Web Application Using Jenkins

---
+++ src/main/java/com/yazidproject/javawebappjenkins/JavaWebAppJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.yazidproject.javawebappjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWebAppJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaWebAppJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yazidproject/javawebappjenkins/JavaWebAppJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yazidproject.javawebappjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaWebAppJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
